c71673b81149f982191dc8be086c7727c66d4762
Billy Chen <billy.chen1206@gmail.com>
2019-05-15 14:57:10 -0400

    text box

--- more.js
+++ more.js
@@ -20,4 +20,33 @@ class Message {
 user1 = new User('billy@example.com', '111')
 user2 = new User('amy@example.com', '111')
 
+let user1form = document.getElementById('user1-form')
+let user2inbox = document.getElementById('user2-inbox')
+window.addEventListener('keydown', function(event) {
+  if(event.which == 13) {
+    sendMessage()
+  }
+})
+
+function sendMessage() {
+  let text = document.getElementById('msg-content')
+  let p = document.createElement('p')
+  p.innerText = text.value
+  user2inbox.appendChild(p)
+  text.value = ''
+  text.focus()
+}
+
+user1form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  sendMessage()
+})
+
+
+
+
+
+
+
+
 // msg = new Message('Why do the Knicks suck?')
